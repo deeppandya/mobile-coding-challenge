74e7bdce1fa612f70a81acc9fe6e40ba2e5c43db
deeppandya <deeppanya91@gmail.com>
2017-11-28 10:18:57 -0500

    - added ViewModel and repository - updated java sdk to 8

---
+++ app/src/main/java/mainpackage/traderevtest/datarepository/UnsplashPhotosRepository.java
@@ -0,0 +1,53 @@
+package mainpackage.traderevtest.datarepository;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.util.Log;
+import java.util.List;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+import mainpackage.traderevtest.BuildConfig;
+import mainpackage.traderevtest.model.UnsplashPhoto;
+import mainpackage.traderevtest.networkcalls.UnsplashPhotoService;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by deepp on 2017-11-28.
+ */
+
+public class UnsplashPhotosRepository {
+
+    private UnsplashPhotoService unsplashPhotoService;
+    private static UnsplashPhotosRepository unsplashPhotosRepository;
+
+    private UnsplashPhotosRepository() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BuildConfig.UNSPLASH_END_POINT)
+                .build();
+
+        unsplashPhotoService = retrofit.create(UnsplashPhotoService.class);
+    }
+
+    public synchronized static UnsplashPhotosRepository getInstance() {
+        if (unsplashPhotosRepository == null) {
+            unsplashPhotosRepository = new UnsplashPhotosRepository();
+        }
+        return unsplashPhotosRepository;
+    }
+
+    public LiveData<List<UnsplashPhoto>> getUnsplashPhotos(String page, String perPage, String orderBy) {
+        final MutableLiveData<List<UnsplashPhoto>> data = new MutableLiveData<>();
+        unsplashPhotoService.getUnsplashPhotos(BuildConfig.UNSPLASH_CLIENT_ID, page, perPage, orderBy).subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(unsplashPhotos -> {
+                    Log.e("User", unsplashPhotos.get(0).getUser().getName());
+                    data.setValue(unsplashPhotos);
+                });
+        return data;
+    }
+}
+
---
+++ app/src/main/java/mainpackage/traderevtest/viewmodel/UnsplashPhotosViewModel.java
@@ -0,0 +1,25 @@
+package mainpackage.traderevtest.viewmodel;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+import java.util.List;
+
+import mainpackage.traderevtest.datarepository.UnsplashPhotosRepository;
+import mainpackage.traderevtest.model.UnsplashPhoto;
+
+/**
+ * Created by deepp on 2017-11-28.
+ */
+
+public class UnsplashPhotosViewModel extends ViewModel {
+
+    private UnsplashPhotosRepository unsplashPhotosRepository;
+
+    public UnsplashPhotosViewModel() {
+        unsplashPhotosRepository = UnsplashPhotosRepository.getInstance();
+    }
+
+    public LiveData<List<UnsplashPhoto>> getUnsplashPhotos(String page,String perPage,String orderBy) {
+        return unsplashPhotosRepository.getUnsplashPhotos(page,perPage,orderBy);
+    }
+}
